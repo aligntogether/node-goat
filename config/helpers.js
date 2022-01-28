034e0707b0d8b9225eb9a90980caeada6a40b2e8
Danish <dkhan9591@gmail.com>
2022-01-28 23:34:30 +0530

    logs

--- config/helpers.js
+++ config/helpers.js
@@ -3,7 +3,7 @@ const fsWriteLog = (data) => {
   console.log("error data", data);
   var logStream = fs.createWriteStream("log.txt", { flags: "a" });
   // use {flags: 'a'} to append and {flags: 'w'} to erase and write a new file
-  logStream.write(`\n\n <br> ${new Date()} #>`);
+  logStream.write(`\n\n${new Date().toISOString()} #> `);
   logStream.write(data.toString());
   logStream.end("\n \n ");
 };
